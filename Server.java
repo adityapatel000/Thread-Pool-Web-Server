8dd129aa0daa4197ecfb9f0107598c62232877ce
adityapatel000 <141441968+adityapatel000@users.noreply.github.com>
2026-02-10 20:40:56 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,52 @@
+package PROJECTS.WebServer.ThreadPool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final ExecutorService threadPool;
+
+    public Server(int poolSize) {
+        this.threadPool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void handleClient(Socket clientSocket) {
+        try(PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)){
+            toSocket.println("Hellow from Server " + clientSocket.getInetAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8010;
+        int poolSize = 10;
+
+        Server server = new Server(poolSize);
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(70000);
+            System.out.println("server is listing on port " + port);
+
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                server.threadPool.execute(() -> {
+                    server.handleClient(clientSocket);
+                });
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            server.threadPool.shutdown();
+        }
+    }
+}
